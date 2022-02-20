f6f4d205f0405b22fb2513b63406713a414882a9
bthank <bthankachan@yahoo.com>
2022-02-20 15:55:11 -0500

    initial commit

---
+++ ratingsdataservice/ratingsdataservice/src/main/java/io/javabrains/ratingsdataservice/RatingsdataserviceApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsdataserviceApplication.class, args);
+	}
+
+}
